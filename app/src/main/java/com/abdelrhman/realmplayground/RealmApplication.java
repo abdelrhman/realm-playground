214029fbcb0f3a5ed74244ac4402ec8ca62c4118
Abdelrahman Talat <abdelrhmantalat.93@gmail.com>
2017-04-11 00:08:57 +0200

    add and init realm

--- app/src/main/java/com/abdelrhman/realmplayground/RealmApplication.java
+++ app/src/main/java/com/abdelrhman/realmplayground/RealmApplication.java
@@ -3,6 +3,8 @@ package com.abdelrhman.realmplayground;
 import android.app.Application;
 import android.os.StrictMode;
 
+import io.realm.Realm;
+
 /**
  * @auther abdelrhman on 10/04/17.
  */
@@ -12,6 +14,7 @@ public class RealmApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Realm.init(this);
         if (BuildConfig.DEBUG) {
             StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
                     .detectAll()
