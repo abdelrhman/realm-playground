06050b36fd8a21f1e3a5c17c35255daab63c4448
Abdelrahman Talat <abdelrhmantalat.93@gmail.com>
2017-04-17 22:51:55 +0200

    getting ready

---
+++ app/src/main/java/com/abdelrhman/realmplayground/jsonexample/JsonExampleActivity.java
@@ -0,0 +1,16 @@
+package com.abdelrhman.realmplayground.jsonexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.abdelrhman.realmplayground.R;
+
+
+public class JsonExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_json_example);
+    }
+}
